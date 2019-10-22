f83e495cb6946eb66a66c24feae7c862bd5a8e78
Shamel Lakin <christlife4u@gmail.com>
2019-10-22 17:38:08 +0000

    changes again

--- lib/lamborghini/cli.rb
+++ lib/lamborghini/cli.rb
@@ -1,10 +1,34 @@
 class Lamborghini::Cli
   
-  # def 
-  #   puts "Code Happy Buy Lambo's:"
-  # end 
+ 
   def call 
     puts "VRrrooooom!!"
+    list_cars
+    menu
+    addio
   end 
+  
+  def list_cars
+    puts <<-Doc.gsub /^\s*/, ''
+    1. Aventador  price  information 
+    2. Huracan  price  information 
+    Doc
+  end 
+  
+  def menu 
+    puts "Please enter the number of your supercar:"
+    input = gets.strip
+    case input 
+    when "1"
+      puts "Aventador information"
+    when "2"
+      puts "Huracan information"
+    end 
+    
+  end 
+  
+  def addio 
+  end
+    
 
 end 
