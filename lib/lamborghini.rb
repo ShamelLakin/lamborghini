9a089c91ec57541953e14adf905a1e16460432c1
Shamel Lakin <christlife4u@gmail.com>
2019-10-22 13:51:51 +0000

    first commit

---
+++ lib/lamborghini.rb
@@ -0,0 +1,6 @@
+require "lamborghini/version"
+
+module Lamborghini
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lamborghini/version.rb
@@ -0,0 +1,3 @@
+module Lamborghini
+  VERSION = "0.1.0"
+end
