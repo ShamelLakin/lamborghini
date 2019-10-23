8ca49559656f797ad21ba22b9c3bd4e5a6c625a4
Shamel Lakin <christlife4u@gmail.com>
2019-10-23 15:28:14 +0000

    new change

--- lib/lamborghini/cli.rb
+++ lib/lamborghini/cli.rb
@@ -9,29 +9,32 @@ class Lamborghini::Cli
   end 
   
   def list_cars
-    puts <<-Doc.gsub /^\s*/, ''
-    1. Aventador  price  information 
-    2. Huracan  price  information 
-    Doc
+    # puts <<-Doc.gsub /^\s*/, ''
+    # 1. Aventador  price  information 
+    # 2. Huracan  price  information 
+    # Doc
     @lambos = Lamborghini::Cars.now
+    @lambos.each.with_index(1) do |car, i|
+      puts "#{i}. #{car.name} - #{car.price} - #{car.engine}"
+    end
   end 
   
   def menu 
     input = nil
-    # while != "exit"
+    while input != "exit"
     puts "Please enter the number of your supercar or type list or exit:"
+    
     input = gets.strip.downcase
-    case input 
-    when "1"
-      puts "Aventador information"
-    when "2"
-      puts "Huracan information"
-    when "list"
-        puts list_cars
-      else 
+    if input.to_1 >  0
+      puts @lambos[input.to_i -1]
+       elsif input == "list"
+        list_cars
+       elsif input == "exit"
+        puts addio
+       else 
         puts "Oops! you've hit a bump in the road, Please type list or exit"
     end 
-    
+   end
   end 
   
   def addio 
